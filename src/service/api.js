54a49117527f838479df84f49b1e8bd83e98d251
Pavan Kumar <pavankkarkera480@gmail.com>
2025-05-03 12:11:57 +0530

    for category list api call

--- src/service/api.js
+++ src/service/api.js
@@ -4,3 +4,5 @@ export const fetchProducts = axios.get('https://fakestoreapi.com/products');
 
 export const fetchProductDetail = (productId) => axios.get(`https://fakestoreapi.com/products/${productId}`);
 
+export const fetchCategoryList = axios.get('https://fakestoreapi.com/products/categories');
+
