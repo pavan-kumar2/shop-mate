be2c2bafa0b0561bc41de50a01c57d6a10e05dd0
Pavan Kumar <pavankkarkera480@gmail.com>
2025-05-04 00:12:45 +0530

    banner data update

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,19 +3,42 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import images from "../utils/images";
 import { Autoplay, EffectCoverflow } from "swiper/modules";
+import { Link } from "react-router-dom";
 
 const DESKTOP_BANNER = [
-  images.banner1,
-  images.banner2,
-  images.banner3,
-  images.banner4,
+  { id: "3", imgUrl: images.banner1, title: "Mens Cotton Jacket" },
+  { id: "6", imgUrl: images.banner2, title: "Solid Gold Petite Micropave" },
+  {
+    id: "17",
+    imgUrl: images.banner3,
+    title: "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+  },
+  {
+    id: "14",
+    imgUrl: images.banner4,
+    title:
+      "Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED",
+  },
 ];
 
 const MOBILE_BANNER = [
-  images.mobileBanner1,
-  images.mobileBanner2,
-  images.mobileBanner3,
-  images.mobileBanner4,
+  { id: "3", imgUrl: images.mobileBanner1, title: "Mens Cotton Jacket" },
+  {
+    id: "6",
+    imgUrl: images.mobileBanner2,
+    title: "Solid Gold Petite Micropave",
+  },
+  {
+    id: "17",
+    imgUrl: images.mobileBanner3,
+    title: "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+  },
+  {
+    id: "14",
+    imgUrl: images.mobileBanner4,
+    title:
+      "Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED",
+  },
 ];
 
 export default function HeroBanner() {
@@ -49,12 +72,20 @@ export default function HeroBanner() {
         modules={[Autoplay, EffectCoverflow]}
       >
         {heroBanner.map((banner, i) => (
-          <SwiperSlide key={i} className="rounded-b-xl overflow-hidden">
-            <img
-              className=" w-full object-fill"
-              src={banner}
-              alt={"banner-image" + i}
-            />
+          <SwiperSlide
+            key={"banner" + i}
+            className="rounded-b-xl cursor-pointer overflow-hidden"
+          >
+            <Link
+              to={`/product/${encodeURIComponent(banner.title)}`}
+              state={{ productId: banner.id }}
+            >
+              <img
+                className=" w-full object-fill"
+                src={banner.imgUrl}
+                alt={"banner-image" + i}
+              />
+            </Link>
           </SwiperSlide>
         ))}
       </Swiper>
